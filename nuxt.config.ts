791d8c2d3c3d87799f6fcc7ffc108299dbf467db
jonathanndinga <ndingajonathan96@gmail.com>
2025-02-13 14:02:07 +0100

    pushing the valentine website to its remote repo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,29 @@
 import tailwindcss from "@tailwindcss/vite";
-export default defineNuxtConfig({  
-  compatibilityDate: "2024-11-01",  
-  devtools: { enabled: true }, 
-  css: ['~/assets/css/main.css'],
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Priscillia",
+      meta: [
+        {
+          name: "description",
+          content: "Happy valentine day mi amor",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
+    },
+  },
+  compatibilityDate: "2025-02-13",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
   head: {
     link: [
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap' },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap",
+      },
     ],
-  }, 
-  vite: {    
-    plugins: [      
-      tailwindcss(),    
-    ],  
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
